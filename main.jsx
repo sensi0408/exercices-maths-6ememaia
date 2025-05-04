89bd75c7505c313298c56c146190483dede8da45
sensi0408 <sofyanchikhaoui2@gmail.com>
2025-05-04 11:56:49 +0200

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ExercicesMaths6eme from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ExercicesMaths6eme />
+  </React.StrictMode>
+);
